0fe7f325290e1c1f582df08e2da75d2f26b9b29d
mmm12-create <2440285774@qq.com>
2025-03-17 17:55:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小猪',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyGwu6jYy5YI66tr8N6MdZ6fjh90',
+      id: 'oyGwu6tz7G8gJo2_mgsZn3LUJOA4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'OQfcvwtK-vbaFFFJzwl1H91jtGcl7gSsuIULRU4qxWY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
