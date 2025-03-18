35e78b0c5455650e01d5bd3ef2dd357f0ee492fa
mmm12-create <2440285774@qq.com>
2025-03-18 11:53:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oyGwu6tz7G8gJo2_mgsZn3LUJOA4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'OQfcvwtK-vbaFFFJzwl1H91jtGcl7gSsuIULRU4qxWY',
+      useTemplateId: 'oKST407u1H57OIBJgAQlX9aZVGlP652v9pljavZ7P3o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-15',
       festivals: [
